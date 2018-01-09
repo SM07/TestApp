46d138c56f6269256fa158ce9939c2c33cbb0a2b
Saqib Mushtaq <saqib.mushtaq@voa.gsi.gov.uk>
2018-01-09 15:26:03 +0000

    Implement saving a gig

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -17,6 +19,7 @@ namespace GigHub.Controllers
 
 
         }
+        [Authorize]
         // GET: Gigs
         public ActionResult Create()
         {
@@ -29,6 +32,39 @@ namespace GigHub.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]  // declare an attribute to make sure authenicated users can call this action
+        [HttpPost]  // want this action only to be called by HttpPost 
+
+        // Adding a new object with Entity Framework is as simple as constructing a new instance of your object 
+        //and registering it using the Add method on DbSet.
+
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+
+            //var artistId = User.Identity.GetUserId();
+            //var artist = _context.Users.Single(u => u.Id == artistId);
+            //var genre = _context.Genres.Single(u => u.Id == viewModel.Genre);
+
+            var gig = new Gig
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                GenreId = viewModel.Genre,
+                Venue = viewModel.Venue
+
+            };
+
+            //we have a gig objext which we will need to add to our context
+            _context.Gigs.Add(gig);
+
+            // entity framework will generate a sql statement and execute it against my database
+            _context.SaveChanges();
+            // After save redirect user to home page
+            return RedirectToAction("Index", "Home");
+
+
         }
     }
 }
