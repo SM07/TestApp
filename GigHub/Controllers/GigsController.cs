9daa3be215be41e67610642f2d74f04fdccc58b5
Saqib Mushtaq <saqib.mushtaq@voa.gsi.gov.uk>
2018-01-09 12:20:54 +0000

    Create Add a Gig form

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,34 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        //initialise a constructor
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+
+
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            //create a view model and set its Genre properties
+
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+
+            };
+
+            return View(viewModel);
+        }
+    }
+}
